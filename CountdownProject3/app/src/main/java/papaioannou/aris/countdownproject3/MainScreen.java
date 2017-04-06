6397ec796f15b444fe451da93d6422f5291355e1
Aristotelis Papaioannou <adpapaio@UMICH.EDU>
2017-04-06 17:39:59 -0400

    Some stuff

--- CountdownProject3/app/src/main/java/papaioannou/aris/countdownproject3/MainScreen.java
+++ CountdownProject3/app/src/main/java/papaioannou/aris/countdownproject3/MainScreen.java
@@ -1,3 +1,7 @@
+/* Created by: Aristotelis Papaioannou
+ * CIS 436 - Project 3
+*/
+
 package papaioannou.aris.countdownproject3;
 
 import android.graphics.Color;
@@ -5,11 +9,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -21,6 +21,8 @@ import org.w3c.dom.Text;
 import java.util.ArrayList;
 
 public class MainScreen extends AppCompatActivity {
+
+    int countdownSeconds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,72 +38,94 @@ public class MainScreen extends AppCompatActivity {
             }
         });
 
-        Spinner spinner = (Spinner) findViewById(R.id.HighestSpinner);
-        spinner.setEnabled(false);
+        Spinner spinner = (Spinner) findViewById(R.id.HighestSpinner); //Start the highest notification spinner disabled
+        spinner.setEnabled(false);                                     //
     }
 
 
+    //When Set Countdown button is clicked
     public void SetCountdownClick(View view)
     {
         EditText countdownBox = (EditText) findViewById(R.id.timeToNumb); //Box they set Time to Countdown
         String errorText = "Text Box was left Blank"; //Error Message;
         int seconds;
+
         if (countdownBox.getText().toString().length() != 0 ) //If the Text box is not empty
         {
-            if(countdownBox.getText().toString().length() > 2)
-            {
-                seconds = Integer.valueOf(countdownBox.getText().toString().substring(0, 3)); //Only get the first 3 chars in the string (Limits it from not making the int over the int.max val
-            }
-            else
-            {
-                seconds = Integer.valueOf(countdownBox.getText().toString());
-            }
+            seconds = getTheSeconds(countdownBox);
 
-            seconds = validateNumber(seconds, countdownBox); //Make sure the number is not over 120 or less than 0
-            populateSpinner(seconds);
+            populateSpinner(seconds); //set up the highest notification spinner
+
+            countdownSeconds = seconds;
         }
         else
         {
             countdownBox.setHint(errorText); //Set the Error text to red
             countdownBox.setHintTextColor(Color.RED); //
         }
+
+
     }
 
 
+    //Get the seconds from the textbox
+    private int getTheSeconds(EditText countdownBox)
+    {
+        int seconds;
 
+        if(countdownBox.getText().toString().length() > 2)
+        {
+            seconds = Integer.valueOf(countdownBox.getText().toString().substring(0, 3)); //Only get the first 3 chars in the string (Limits it from not making the int over the int.max val
+        }
+        else
+        {
+            seconds = Integer.valueOf(countdownBox.getText().toString()); //Get the whole value of the string
+        }
+
+        seconds = validateNumber(seconds, countdownBox); //Make sure the number is not over 120 or less than 0
+
+        return seconds;
+
+    }
+
+
+    //Make sure the seconds is not over 120 or less than 5
+    //Will also make the seconds a multiple of 5
+    //is passed in seconds and the box that the user entered seconds in
     private int validateNumber(int seconds, EditText countdownBox)
     {
-
-        if(seconds > 120)
+        if(seconds > 120) //is seconds over 120
         {
             seconds = 120;
             Toast.makeText(this, "120 is the Max", Toast.LENGTH_SHORT).show();
         }
-        else if (seconds < 5)
+        else if (seconds < 5) //is seconds less than 5
         {
             seconds = 5;
             Toast.makeText(this, "5 is the Min", Toast.LENGTH_SHORT).show();
         }
-        else
+        else //otherwise make it a multiple of 5
         {
             seconds = 5*(Math.round(seconds/5));
         }
 
         countdownBox.setText(seconds + ""); //set the text to the seconds
-        countdownBox.setTextColor(Color.BLACK);
+        countdownBox.setTextColor(Color.BLACK); //change the text to black
         return seconds;
     }
 
     //fills the spinner with the Highest Notification values
+    //is passed in seconds
     private void populateSpinner(int seconds)
     {
         ArrayList<String> highNotif = new ArrayList<String>(); //Array list for Highest Notifications
         Spinner hnSpinner = (Spinner) findViewById(R.id.HighestSpinner); //Setting the Highest Notification spinner
         TextView hnTextView = (TextView) findViewById(R.id.highNot_text);
+
         String[] hnVals = {"1","5","10","20","30","60","90"}; //7 values for highest notification (7 in for loop)
         int hfLength = 6; //Default value will be to add all highest values to notification
 
-        hnSpinner.setEnabled(true);
+        hnSpinner.setEnabled(true); //enable the spinner
         hnTextView.setTextColor(Color.BLACK);
 
         if(seconds > 90) //If seconds is higher than 90 seconds
@@ -136,13 +160,38 @@ public class MainScreen extends AppCompatActivity {
 
         for (int i = 0; i < hfLength; i++) //Populate the spinner based on how many notfications it needs
         {
-            highNotif.add(" " + hnVals[i] + " Second(s)");
+            highNotif.add(" " + hnVals[i] + " Second(s)"); //add it to the spinner
         }
 
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, highNotif);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
         hnSpinner.setAdapter(adapter);
+
+    }
+
+    public void startCountdown(View view)
+    {
+        TextView messageBox = (TextView) findViewById(R.id.message_text_box);
+        EditText countdownBox = (EditText) findViewById(R.id.timeToNumb); //Box they set Time to Countdown
+
+        int seconds = countdownSeconds;
+
+        if(messageBox.getText().toString().length() != 0)
+        {
+            Toast.makeText(this, "Countdown for "+ messageBox.getText().toString() +" has started", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, seconds+ "", Toast.LENGTH_SHORT).show();
+
+        }
+        else
+        {
+            Toast.makeText(this, "Please Enter a Message", Toast.LENGTH_SHORT).show();
+            messageBox.setHint("Enter Message Here");
+            messageBox.setHintTextColor(Color.RED);
+        }
+
+
     }
 
 }
